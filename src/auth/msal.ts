b28a60b320b2d319c11251daa3e2743a3b6a20bf
Shai Petel <shai@kwizcom.com>
2024-03-04 14:46:32 -0500

    updated dependencies

--- src/auth/msal.ts
+++ src/auth/msal.ts
@@ -1,15 +1,15 @@
-import * as msal from "@azure/msal-node";
+import { ConfidentialClientApplication } from "@azure/msal-node";
 import { AuthContextType, AuthenticationModes, ITenantInfo } from "@kwiz/common";
 //find tenant id? https://login.microsoftonline.com/kwizcom.onmicrosoft.com/.well-known/openid-configuration
 //https://stackoverflow.com/questions/54771270/msal-ad-token-not-valid-with-sharepoint-online-csom
 
-var apps: { [tenant: string]: msal.ConfidentialClientApplication } = {};
+var apps: { [tenant: string]: ConfidentialClientApplication } = {};
 
 function GetApp(tenantInfo: ITenantInfo, auth: AuthContextType) {
     let key = `${tenantInfo.idOrName}|${auth.authenticationMode}`
     if (!apps[key]) {
         auth.authenticationMode === AuthenticationModes.clientSecret
-            ? apps[key] = new msal.ConfidentialClientApplication({
+            ? apps[key] = new ConfidentialClientApplication({
                 auth: {
                     clientId: auth.clientId,
                     authority: tenantInfo.authorityUrl,
@@ -17,7 +17,7 @@ function GetApp(tenantInfo: ITenantInfo, auth: AuthContextType) {
                 },
 
             })
-            : apps[key] = new msal.ConfidentialClientApplication({
+            : apps[key] = new ConfidentialClientApplication({
                 auth: {
                     clientId: auth.clientId,
                     authority: tenantInfo.authorityUrl,
