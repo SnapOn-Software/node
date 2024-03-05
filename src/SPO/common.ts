3cd5fd1ae38c516d7368ee2ac96c454d702bdadb
Shai Petel <shai@kwizcom.com>
2024-03-05 17:09:41 -0500

    repackage to test devops build error

--- src/SPO/common.ts
+++ src/SPO/common.ts
@@ -1,12 +1,12 @@
-import { GetMSALSiteScope, isNullOrUndefined } from "@kwiz/common";
+import { AuthContextType, GetMSALSiteScope, ITenantInfo, isNullOrUndefined } from "@kwiz/common";
 import { GetMSALToken } from "../auth/msal";
 import { getAxiosConfigBearer } from "../axios";
 
-var auth: any = null;
-export function ConfigureSPOAuth(config?: any) {
+var auth: AuthContextType = null;
+export function ConfigureSPOAuth(config?: AuthContextType) {
     auth = config;
 }
-export async function getAxiosConfigSharePoint(tenantInfo: any, hostName: string) {
+export async function getAxiosConfigSharePoint(tenantInfo: ITenantInfo, hostName: string) {
     if (isNullOrUndefined(auth)) throw Error("Call ConfigureSPOAuth first");
     // only certificate supported
     // DisableCustomAppAuthentication property, that disable this kind of auth., however it can be overriden using this command:
