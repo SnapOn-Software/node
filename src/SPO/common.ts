6af1e6499a336f67021aa14fb95de09f482a6cf9
Shai Petel <shai@petel.com>
2024-03-05 16:19:56 -0500

    trying exports with no common types

--- src/SPO/common.ts
+++ src/SPO/common.ts
@@ -1,12 +1,12 @@
-import { AuthContextType, GetMSALSiteScope, ITenantInfo, isNullOrUndefined } from "@kwiz/common";
+import { GetMSALSiteScope, isNullOrUndefined } from "@kwiz/common";
 import { GetMSALToken } from "../auth/msal";
 import { getAxiosConfigBearer } from "../axios";
 
-var auth: AuthContextType = null;
-export function ConfigureSPOAuth(config?: AuthContextType) {
+var auth: any = null;
+export function ConfigureSPOAuth(config?: any) {
     auth = config;
 }
-export async function getAxiosConfigSharePoint(tenantInfo: ITenantInfo, hostName: string) {
+export async function getAxiosConfigSharePoint(tenantInfo: any, hostName: string) {
     if (isNullOrUndefined(auth)) throw Error("Call ConfigureSPOAuth first");
     // only certificate supported
     // DisableCustomAppAuthentication property, that disable this kind of auth., however it can be overriden using this command:
