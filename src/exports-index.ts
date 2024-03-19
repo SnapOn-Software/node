7bbc65535a814b67cff82c98b3a4d5308de44b30
Shai Petel <shai@petel.com>
2024-03-19 10:39:57 -0400

    exported new types

--- src/exports-index.ts
+++ src/exports-index.ts
@@ -1,4 +1,5 @@
 export * from './auth/exports-index';
 export * from './graph/exports-index';
 export * from './spo/exports-index';
+export * from './storage/exports-index';
 export * from './axios';
